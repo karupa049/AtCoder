f60e58858d2713a2a25f173e511c048d523ba24b
karupa049 <23j5049@stu.meisei-u.ac.jp>
2026-03-22 17:36:57 +0900

    1-7まで終了

---
+++ CPP/chap1/1.04.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int name;
+    
+    name = 10;
+
+    cout << name << endl;
+    cout << name + 2 << endl;
+    cout << name * 3 << endl;
+
+    int a = 10, b = 5;
+
+    
+    b = a;
+    a = 5;
+    cout << a << endl;
+    cout << b << endl;
+
+    int i = 10;
+    double d = 0.5;
+    string s = "Hello";
+
+    cout << i << endl;
+    cout << d << endl;
+    cout << s << endl;
+
+    int sakamoto;
+    cout << sakamoto << endl;
+    
+}
---
+++ CPP/chap1/1.05.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    /*
+    int name ;
+
+    name = 10;
+
+    cout << name << endl;
+
+    name = 5;
+
+    cout << name << endl;
+
+    int a;
+
+    cin >> a;
+
+    cout << a * 10 << endl;
+
+    string text;
+    double d;
+    cin >> text;
+    cin >> d;
+    cout << text << ", " << d << endl;
+    */
+
+    int x, y, z;
+    cin >> x >> y >> z;
+    cout << x * y * z;
+
+
+}
