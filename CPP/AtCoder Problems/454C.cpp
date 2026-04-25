f71ccf55a0cfa85669287273ce690d2539f63ef8
karupa049 <23j5049@stu.meisei-u.ac.jp>
2026-04-25 20:14:38 +0900

    練習

---
+++ CPP/AtCoder Problems/454C.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+#include<queue>
+using namespace std;
+
+int main(){
+    int n, m;       //友達が何人いてアイテムが何種類あるか、（アイテム1を含む）
+    cin >> n >> m;
+
+    vector<vector<int>> graph(n + 1);
+
+    /*
+    1 2
+    2 3
+    3 4
+    2 4
+    5 2
+    */
+   for(int i = 0; i < m; i++){
+    int a, b;
+    cin >> a >> b;
+
+    graph[a].push_back(b);
+   }
+
+   queue<int> q;
+   vector<bool> visited(n + 1, false);
+
+   q.push(1);
+   visited[1] = true;
+
+   while(!q.empty()){
+    int current = q.front();
+    q.pop();
+
+    for(int nextItem : graph[current]){
+        if(!visited[nextItem]){
+            visited[nextItem] = true;
+            q.push(nextItem);
+        }
+    }
+   }
+
+   int ans = 0;
+    for (int i = 1; i <= n; ++i) {
+        if (visited[i]) {
+            ans++;
+        }
+    }
+
+    cout << ans << endl;
+}
