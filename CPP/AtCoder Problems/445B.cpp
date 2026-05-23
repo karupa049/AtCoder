815419c12cd987e672ef3c250f598e31eca6347f
karupa049 <23j5049@stu.meisei-u.ac.jp>
2026-05-23 23:16:58 +0900

    終了

--- CPP/AtCoder Problems/445B.cpp
+++ CPP/AtCoder Problems/445B.cpp
@@ -7,16 +7,28 @@ int main(){
 
     vector<string> s(n);
     for(int i = 0; i < n; i++) cin >> s[i];
-    for(int i = 0; i < n; i++) cout << s[i] << " "; cout << endl;
+    // for(int i = 0; i < n; i++) cout << s[i] << " "; cout << endl;
 
     int max = s[0].length();
-    cout << max << endl;
+    // cout << max << endl;
 
     for(int i = 1; i < n; i++){
         if(s[i].length() > max){
             max = s[i].length();
         }
     }
-    cout << max << endl;
+    // cout << max << endl;
 
+    for(int i = 0; i < s.size(); i++){
+        int len = (max - s[i].length()) / 2;
+        // cout << len << endl;
+        string kigou;
+
+        for(int j = 0; j < len; j++){
+            kigou += '.';
+        }
+
+        cout << kigou << s[i] << kigou << endl;
+
+    }
 }
